4a3547c10249637a1f059ab4bfc507e7c3c3ab50
estiglemanDrift <estigleman@drift.com>
2022-03-28 10:19:18 -0400

    first comment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const port = 6500;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/main", (req, res) => {
-  res.render("main");
+  res.render("main.ejs");
 });
 
 app.get("/identify", (req, res) => {
